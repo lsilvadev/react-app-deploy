fadd937840295a6137c025ee7903c2a1c869e4e0
Leticia Silva <leticia.silva@acessodigital.com.br>
2020-03-31 20:04:45 -0300

    logo animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LogoAnimation from "./LogoAnimation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <LogoAnimation />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
